0bae69844855ee946f2e6e7370e5e8844473ca24
Edwin574 <owinoedwin574@gmail.com>
2023-01-08 23:07:40 +0300

    working on endpoints

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -1,3 +1,4 @@
+
 from rest_framework import routers
 from .views import TodoViewSet
 
@@ -6,3 +7,4 @@ router= routers.DefaultRouter()
 router.register(r'api/todos',TodoViewSet,'todos')
 
 urlpatterns = router.urls
+
