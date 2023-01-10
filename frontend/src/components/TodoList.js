1857f2eb7eb445af88118bc4228e783019d584cc
Edwin574 <owinoedwin574@gmail.com>
2023-01-10 22:00:46 +0300

    using modals

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,67 +1,121 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
 
-import { ListGroup } from 'react-bootstrap';
-import {MdCheckBox,MdCheckBoxOutlineBlank,MdEdit,MdDelete} from "react-icons/md"
+import { ListGroup } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { Modal, FormControl } from "react-bootstrap";
+import {
+  MdCheckBox,
+  MdCheckBoxOutlineBlank,
+  MdEdit,
+  MdDelete,
+} from "react-icons/md";
+import { useState } from "react";
 
-function TodoList({ todos = [],setTodos }) {
-    
-    const handleUpdate = async (id, value) => {
-        return (
-            axios.patch(`api/todos/${id}/`, value).then(
-                (res) => {
-                    const { data } = res
-                    const newTodos = todos.map(t => {
-                        if (t.id === id) {
-                            return data;
-                        }
-                        return t
-                    })
-                    setTodos(newTodos)
+function TodoList({ todos = [], setTodos }) {
+  const { show, setShow } = useState(false);
 
-                }
-            ).catch(() => {
-                console.error();
-            })
-        )
-    }
-    
-    const renderListGroupItem = (t) => {
+  const { record, setRecord } = useState(null);
 
-        return (<ListGroup.Item key={t.id} className="d-flex justify-content-between align-items-center  ">
-            <div className='d-flex justify-content-center '>
-                <span style={{ marginRight: '12px', cursor: 'pointer' }}
-                    onClick={() => {
-                        handleUpdate(t.id, {
-                        completed:!t.completed
-                    })
-                }}>
-                    {t.completed===true?<MdCheckBox/>:<MdCheckBoxOutlineBlank/>}
+  const handleClose = () => {
+    setShow(false);
+  };
 
-                </span>
-                <span>
-                    {t.name}
-                </span>
+  const handleUpdate = async (id, value) => {
+    return axios
+      .patch(`api/todos/${id}/`, value)
+      .then((res) => {
+        const { data } = res;
+        const newTodos = todos.map((t) => {
+          if (t.id === id) {
+            return data;
+          }
+          return t;
+        });
+        setTodos(newTodos);
+      })
+      .catch(() => {
+        console.error();
+      });
+  };
 
-            </div>
-            <div>
-                <MdEdit style={{
-                    cursor: 'pointer',
-                    marginRight:'12px'
-                }} />
-                <MdDelete style={{
-                    cursor:'pointer'
-                }} />
-            </div>
-        </ListGroup.Item>)
-    }
-
+  const renderListGroupItem = (t) => {
     return (
-        <ListGroup>
-            {todos.map(renderListGroupItem)} 
-      </ListGroup>
-    )
+      <ListGroup.Item
+        key={t.id}
+        className="d-flex justify-content-between align-items-center  "
+      >
+        <div className="d-flex justify-content-center ">
+          <span
+            style={{ marginRight: "12px", cursor: "pointer" }}
+            onClick={() => {
+              handleUpdate(t.id, {
+                completed: !t.completed,
+              });
+            }}
+          >
+            {t.completed === true ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          </span>
+          <span>{t.name}</span>
+        </div>
+        <div>
+          <MdEdit
+            style={{
+              cursor: "pointer",
+              marginRight: "12px",
+            }}
+            onClick={() => {
+              setRecord(t);
+              setShow(true);
+            }}
+          />
+          <MdDelete
+            style={{
+              cursor: "pointer",
+            }}
+          />
+        </div>
+      </ListGroup.Item>
+    );
+  };
+
+  const handleChange = (e) => {
+    setRecord({
+      ...record,
+      name: e.target.value,
+    });
+  };
+
+  const handleSaveChanges = async () => {
+    await handleUpdate(record.id, { name: record.name });
+    handleClose();
+  };
+
+  return (
+    <div>
+      <ListGroup>{todos.map(renderListGroupItem)}</ListGroup>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Todo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormControl
+            value={record ? record.name : ""}
+            onChange={handleChange}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSaveChanges}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
 
-
-export default TodoList
+export default TodoList;
