d306af54e1bf6f410a575660e7d67af630d7631f
Edwin574 <owinoedwin574@gmail.com>
2023-02-28 22:05:36 +0300

    add task using the form

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -2,26 +2,29 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Button, InputGroup, FormControl, Form } from "react-bootstrap";
 
-function TodoForm() {
+function TodoForm({ todo = [], setTodos }) {
   const [name, setName] = useState("");
 
   const handleChange = (e) => {
     setName(e.target.value);
   };
-  const addTask=(e)=>{
-    e.preventDefault()
-    console.log(e)
+  const addTask = (e) => {
+    e.preventDefault();
+    // console.log(e)
 
-   let newTask={ "name":e.target.value,"completed": false} 
-   axios.post('http://127.0.0.1:8000/task',{newTask}).then((res)=>{
-    console.log(res)
-   }).catch((e)=>{
-     console.log(e)
-   })
-  }
+    let newTask = { name: name, completed: false };
+    axios
+      .post("http://127.0.0.1:8000/tasks/", newTask)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
   return (
-    <Form>
+    <Form onSubmit={addTask}>
       <InputGroup className="mb-4">
         <FormControl
           placeholder="New Todo"
